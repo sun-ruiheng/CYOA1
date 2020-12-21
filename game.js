5f36957266c39ef4fe12b115fed16835d904ac5a
Sun Ruiheng <sunruiheng2001@gmail.com>
2020-12-21 14:44:22 +0800

    made wesley arc, maybe unfinished

--- game.js
+++ game.js
@@ -2,11 +2,18 @@ let gameState = {}
 
 function preload () {
   // load in background and characters
-  this.load.image('bg',     'https://content.codecademy.com/projects/learn-phaser/cyoa/background.png');
-  this.load.image('orc',    './images/kexinandkobi2.jpg');
-  this.load.image('kexin',  './images/kexin1.jpg');
-  this.load.image('sadKX',  './images/sadkexin.jpg');
-  this.load.image('duckKX', './images/duckkexin.jpg');
+  this.load.image('bg',      'https://content.codecademy.com/projects/learn-phaser/cyoa/background.png');
+  this.load.image('kxKobi',  './images/kexinandkobi2.jpg');
+  this.load.image('kexin',   './images/kexin1.jpg');
+  this.load.image('sadKX',   './images/sadkexin.jpg');
+  this.load.image('duckKX',  './images/duckkexin.jpg');
+  this.load.image('kekKX',   './images/laughingkexin.jpg');
+  this.load.image('cheekKX', './images/cheekykexin.jpg');
+  this.load.image('deaddog', './images/deadkobi.jpg');
+
+  this.load.image('wesley',  './images/cutewesley.jpg');
+  this.load.image('wizley',  './images/wesleychan.jpg');
+  this.load.image('wizKyn',  './images/wizleykynan.jpg');
 }
 
 
@@ -148,17 +155,7 @@ function fetchPage(page) {
       narrative: 'Hey, good to meet you. I\'m Xie Ke Xin.',
       options: [
         { option: 'Say Hi to Ke Xin',   nextPage: 2 },
-        { option: 'Ignore this Hoe',   nextPage: 41 },
-      ]
-    },
-
-    {
-      character: 'kexin',
-      page: 41,
-      narrative: 'Uhm. Excuse me?!',
-      options: [
-        { option: 'Ok fine wtf do you want',     nextPage: 2 },
-        { option: 'Continue Ignoring',   nextPage: 3 },
+        { option: 'Ignore this Hoe',   nextPage: 9 },
       ]
     },
 
@@ -167,218 +164,235 @@ function fetchPage(page) {
       page: 2,
       narrative: 'Can you please help me and my bibiboi?',
       options: [
-        { option: 'Your... what?',   nextPage: 5 },
+        { option: 'Your... what?',   nextPage: 3 },
         { option: 'Don\'t think so',   nextPage: 4 },
       ]
     },
 
     {
-      character: 'orc',
-      page: 5,
+      character: 'kxKobi',
+      page: 3,
       narrative: 'My dog... His name is Kobi and he\'s very sick.',
       options: [
-        { option: 'How so?',   nextPage: 6 },
-        { option: 'No I don\'t think so',   nextPage: 4 },
+        { option: 'How so?',   nextPage: 4 },
+        { option: 'Not my problem. Fuck off bitch!!!!',   nextPage: 11 },
       ]
     },
 
     {
-      character: 'orc',
-      page: 6,
+      character: 'kxKobi',
+      page: 4,
       narrative: 'He\'s overweight as fuck, and chokes and gags and wheezes in his sleep every night.',
       options: [
-        { option: 'Laugh at Kobi for being obese',   nextPage: 48 },
-        { option: 'And why is he this way?', nextPage: 10}
+        { option: 'Laugh at Kobi for being obese',   nextPage: 7 },
+        { option: 'And why is he this way?', nextPage: 5}
       ]
     },
 
     {
-      character: 'orc',
-      page: 48,
-      narrative: 'Ke Xin is angry and upset, and decides to abandon Kobi. Kobi dies immediately. Ke Xin now hates you.',
-      options: [
-        { option: 'umm... lol ok',   nextPage: 1 } //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      ]
-    },
-
-    {
-      character: 'orc',
-      page: 10,
+      character: 'kxKobi',
+      page: 5,
       narrative: 'He\'s a greedy, lazy bastard haha. Any fatter and he\'s gonna explode.',
       options: [
-        { option: 'No problem',   nextPage: 1 }, //11
-        { option: 'Really?',   nextPage: 1 }, //18
+        { option: 'Oh shit watch out!!!',   nextPage: 6 }
       ]
     },
 
     {
-      character: 'orc',
-      page: 18,
-      narrative: 'Orc: Uhm. Yes? I\'m an orc? Did you miss that somehow?',
+      character: 'deaddog',
+      page: 6,
+      narrative: 'BOOOOM! Kobi implodes! He\'s super DEAD!!!',
       options: [
-        { option: 'I guess so',   nextPage: 20 },
+        { option: 'O shit... haha...',   nextPage: 8 }
+      ]
+    },
+    
+    {
+      character: 'deaddog',
+      page: 7,
+      narrative: 'Ke Xin is angry and upset, and decides to abandon Kobi. Kobi dies immediately.',
+      options: [
+        { option: 'umm... wtf???',   nextPage: 8 } 
       ]
     },
 
     {
-      character: 'orc',
-      page: 20,
-      narrative: 'You and the orc discuss ogre politics while he bakes cookies. You have a perfectly wonderful time.',
+      character: 'kekKX',
+      page: 8,
+      narrative: 'Omg nooo kobi... haha at least the burden is gone lmao fatass dog xD',
       options: [
-        { option: 'Eat cookies',   nextPage: 21 },
+        { option: 'Haha hell yea',   nextPage: 13 } //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! onto wesley
       ]
     },
 
     {
-      character: 'orc',
-      page: 21,
-      narrative: 'The cookies are fantastic. ',
+      character: 'kexin',
+      page: 9,
+      narrative: 'Uhm. Excuse me?!',
       options: [
-        { option: 'Request recipe',   nextPage: 19 },
-      ]
-    },
-
-    {
-      character: 'orc',
-      page: 19,
-      narrative: 'The orc shares his recipe with you. You leave the cave feeling energized from the sugar.\nTHE END',
-      options: [
-        { option: 'Play again', nextPage: 1 },
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 11,
-      narrative: 'Knight: Hey. Did you help out that orc?',
-      options: [
-        { option: 'Yes',  nextPage: 12 },
-        { option: 'No',   nextPage: 23 },
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 23,
-      narrative: 'Knight: OK Good.',
-      options: [
-        { option: 'Actually...',  nextPage: 12 },
-        { option: 'Leave',   nextPage: 24 },
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 24,
-      narrative: 'You walk out of the cave. What a weird adventure you went on today!\nTHE END',
-      options: [
-        { option: 'Play again',  nextPage: 1 },
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 12,
-      narrative: 'Knight: You really shouldn\'t have. How will the orc ever learn?',
-      options: [
-        { option: 'I guess.',     nextPage: 14 },
-        { option: 'Who are you?', nextPage: 13 },
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 13,
-      narrative: 'Knight: Just an interested party.',
-      options: [
-        { option: 'Whatever.', nextPage: 25 },
-        { option: 'You\'re right', nextPage: 17 },
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 25,
-      narrative: 'Knight: I know what\'s best for orcs.',
-      options: [
-        { option: 'I guess.', nextPage: 14 },
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 17,
-      narrative: 'You act like nothing is wrong with what the knight is saying.',
-      options: [
-        { option: 'OK', nextPage: 22 },
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 22,
-      narrative: 'But thinking about it later you realize you should\'ve been more proactive.\nGAME OVER',
-      options: [
-        { option: 'Play again', nextPage: 1 },
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 14,
-      narrative: 'Knight: You guess? Come on. An orc should be able to be independent.',
-      options: [
-        { option: 'Cookies',     nextPage: 15 },
-        { option: 'Who are you?', nextPage: 13 },
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 15,
-      narrative: 'You try explaining to the knight that you got cookies and a new friend.',
-      options: [
-        { option: 'OK',     nextPage: 16 },
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 16,
-      narrative: 'But the knight continues being weird and rude for some reason.\nGAME OVER',
-      options: [
-        { option: 'Play again',     nextPage: 1 },
+        { option: 'Ok fine wtf do you want',     nextPage: 2 },
+        { option: 'Continue Ignoring',   nextPage: 10 },
       ]
     },
 
     {
       character: 'duckKX',
-      page: 3,
+      page: 10,
       narrative: 'Pleaseeeeee look I\'m such a qtpie',
       options: [
         { option: 'Ok wtf fine! What?',  nextPage: 2 },
-        { option: 'Walk away slowly',   nextPage: 4 },
+        { option: 'Walk away',   nextPage: 11 },
       ]
     },
 
     {
       character: 'sadKX',
-      page: 4,
+      page: 11,
       narrative: 'Oh wow you really won\'t help a desperate woman. You really are a penishead, I FKING HOPE YOU DIE!!!',
       options: [
-        { option: 'Leave',   nextPage: 1 },
-        { option: 'Pee on her',   nextPage: 7 },
+        { option: 'Walk away',   nextPage: 14 }, ///////////////////
+        { option: 'Pee on her',   nextPage: 12 },
       ]
     },
 
     {
-      character: 'orc',
-      page: 7,
+      character: 'cheekKX',
+      page: 12,
       narrative: 'You pee on her and she kind of enjoys it.',
       options: [
-        { option: 'OK',   nextPage: 1 }, //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        { option: 'Hell yeaaaaa',   nextPage: 15 }, //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       ]
     },
+
+
+
+//wesley arc
+
+    {
+      character: 'wesley',
+      page: 13,
+      narrative: 'Ooh is that a dead dog?',
+      options: [
+        { option: 'Yea...',   nextPage: 13.5 },
+        { option: 'Who tf are you?', nextPage: 16}
+      ]
+    },
+
+    {
+      character: 'wesley',
+      page: 13.5,
+      narrative: 'Hehe, reminds me of dead quails!!!',
+      options: [
+        { option: '...wtf',   nextPage: 16 }
+      ]
+    },
+
+    {
+      character: 'wesley',
+      page: 14,
+      narrative: 'penis',
+      options: [
+        { option: 'Who tf are you?', nextPage: 16}
+      ]
+    },
+
+    {
+      character: 'wesley',
+      page: 15,
+      narrative: 'Pee on me too plsplspls',
+      options: [
+        { option: 'Okay!',   nextPage: 15.5 },
+        { option: 'Who tf are you?', nextPage: 16}
+      ]
+    },
+
+    {
+      character: 'wesley',
+      page: 15.5,
+      narrative: 'yummy yummy',
+      options: [
+        { option: 'nomnom',   nextPage: 16 }
+      ]
+    },
+
+    {
+      character: 'wizley',
+      page: 16,
+      narrative: 'Hey there, I\'m Wesley the Wizard.',
+      options: [
+        { option: '...Sure you are',   nextPage: 19 },
+        { option: 'Can you make my cock longer?', nextPage: 17 }
+      ]
+    },
+    
+    {
+      character: 'wizley',
+      page: 17,
+      narrative: 'Hmm... yea, but Imma have to jack it off.',
+      options: [
+        { option: 'NO PROBLEM!!!',   nextPage: 18 },
+        { option: 'Hell nahhh you ain\'t no wizard!', nextPage: 19}
+      ]
+    },
+    
+    {
+      character: 'wizley',
+      page: 18,
+      narrative: 'Wesley busts your nut with graceful expertise. Your cock grows three inches.',
+      options: [
+        { option: 'Woah!',   nextPage: 21 }
+      ]
+    },
+    
+    {
+      character: 'wizley',
+      page: 19,
+      narrative: 'I\'ll fuckin prove it to you!',
+      options: [
+        { option: 'Okay, do a trick!',   nextPage: 20 },
+        { option: 'Shut the fuck up bitchhhh', nextPage: 20}
+      ]
+    },
+    
+    {
+      character: 'wizKyn',
+      page: 20,
+      narrative: 'Wesley changes the heads into two mofos.',
+      options: [
+        { option: 'Holy cow!',   nextPage: 21 }
+      ]
+    },
+
+    
+    {
+      character: 'wizley',
+      page: 21,
+      narrative: 'Told you bitchhhh',
+      options: [
+        { option: 'Hehe',   nextPage: 1 }
+      ]
+    },
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     {
       character: 'wizard',
@@ -449,126 +463,6 @@ function fetchPage(page) {
     },
 
     {
-      character: 'knight',
-      page: 40,
-      narrative: 'Knight: Hi.',
-      options: [
-        { option: 'Hi friend',   nextPage: 42},
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 42,
-      narrative: 'Knight: Actually I\'m kind of busy.',
-      options: [
-        { option: 'Oh?',   nextPage: 43},
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 43,
-      narrative: 'Knight: But I\'ll call you later when I can.',
-      options: [
-        { option: 'That\'s fine',   nextPage: 44},
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 44,
-      narrative: 'The Knight never calls, and it shouldn\'t really bother you but it does.',
-      options: [
-        { option: 'OK',   nextPage: 45},
-      ]
-    },
-
-    {
-      character: 'knight',
-      page: 45,
-      narrative: 'You failed to make friends in the cave.\nGAME OVER',
-      options: [
-        { option: 'Play again',   nextPage: 1},
-      ]
-    },
-
-    {
-      character: 'wizard',
-      page: 30,
-      narrative: 'Wizard: Oh. Wow. Ok. What do you want to do?',
-      options: [
-        { option: 'Play video games',   nextPage: 31 },
-        { option: 'Small talk',   nextPage: 32 },
-      ]
-    },
-
-    {
-      character: 'wizard',
-      page: 31,
-      narrative: 'You play video games with the wizard.',
-      options: [
-        { option: 'OK',   nextPage: 36 },
-      ]
-    },
-
-    {
-      character: 'wizard',
-      page: 36,
-      narrative: 'The wizard keeps losing to you at video games!',
-      options: [
-        { option: 'OK',   nextPage: 37 },
-      ]
-    },
-
-    {
-      character: 'wizard',
-      page: 37,
-      narrative: 'The wizard is a very sore loser and casts a banishing spell on you! Sheesh!\nTHE END',
-      options: [
-        { option: 'Play again',   nextPage: 1 },
-      ]
-    },
-
-    {
-      character: 'wizard',
-      page: 32,
-      narrative: 'Wizard: How\'s the weather outside?',
-      options: [
-        { option: 'It\'s OK',   nextPage: 33 },
-        { option: 'The worst!',   nextPage: 33 },
-        { option: 'Great!',   nextPage: 33 },
-      ]
-    },
-
-    {
-      character: 'wizard',
-      page: 33,
-      narrative: 'You and the wizard continue engaging in small talk. You learn you grew up not far from each other.',
-      options: [
-        { option: 'OK',   nextPage: 34 },
-      ]
-    },
-
-    {
-      character: 'wizard',
-      page: 34,
-      narrative: 'You have a couple mutual friends, in fact! You decide to hang out again next week.',
-      options: [
-        { option: 'OK',   nextPage: 35 },
-      ]
-    },
-
-    {
-      character: 'wizard',
-      page: 35,
-      narrative: 'You made a valuable friend today!\nTHE END',
-      options: [
-        { option: 'Play again',   nextPage: 1},
-      ]
-    },
-
-    {
       character: 'wizard',
       page: 28,
       narrative: 'Wizard: Really? Seems like you were kinda standoffish to him earlier.',
@@ -585,16 +479,9 @@ function fetchPage(page) {
       options: [
         { option: 'OK',   nextPage: 1 },
       ]
-    },
-
-    {
-      character: 'orc',
-      page: 9,
-      narrative: 'You leave the cave with a new outlook. Your radiant confidence makes a palpable positive impact on your life.\nTHE END',
-      options: [
-        { option: 'Play again',   nextPage: 1 },
-      ]
     }
+
+
    ]
 
   return pages.find(function(e) { if(e.page == page) return e });
